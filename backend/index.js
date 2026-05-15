2c2c5be3e0dcaac20c37c27383c8f6d1e48a34a5
Anurag-singh6 <anuragsingh76000@gmail.com>
2026-05-15 17:12:31 +0530

    again index fixes

--- backend/index.js
+++ backend/index.js
@@ -13,6 +13,11 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
+// log incoming requests and their Origin header to help debug CORS issues
+app.use((req, res, next) => {
+  console.log("[REQ]", req.method, req.originalUrl, "Origin:", req.headers.origin);
+  next();
+});
 app.use(
   cors({
     origin: [
